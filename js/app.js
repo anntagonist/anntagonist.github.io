6092d9523505d51683ec536be680c4c9ff300c7b
anntagonist <andrew.benavides14@alumni.colostate.edu>
2016-10-15 17:21:25 -0600

    Changed the views for all pages, begun working on descriptions of projects

--- js/app.js
+++ js/app.js
@@ -3,20 +3,18 @@ var app = angular.module('antagonistApp', ['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'views/home.html'
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
         })
-        .when('/contact', {
-            templateUrl: 'views/contact.html'
+        .when('/contact-us', {
+            templateUrl: 'views/contact.html',
         })
-        .when('/projects', {
-            templateUrl: 'views/project.html'
-        })
-        .when('/projects/the-cure', {
-            templateUrl: 'views/project.html',
+        .when('/the-cure', {
+            templateUrl: 'views/thecure.html',
             controller: 'theCureCtrl'
         })
-        .when('/projects/warfare', {
-            templateUrl: 'views/project.html',
+        .when('/warfare', {
+            templateUrl: 'views/warfare.html',
             controller: 'warfareCtrl'
         })
         .otherwise('/');
@@ -28,10 +26,6 @@ $(document).ready(function(){
     //Set the page-view to dynamically change based on the headers.height
     $(window).resize(function() {
         $('.container-view').css('margin-top', $('.navbar-fixed-top').height());
-//        var $viewHeight = $('.navbar-fixed-top').height() + $('.navbar-fixed-bottom').height();
-//        $viewHeight = $(window).height() - $viewHeight;
-//        $('.container-view').css('height', $viewHeight);
         $('.container-view').css('height', $(window).height());
-//        $('.project-information').css('margin-left', $('.navbar-fixed-side-left'));
     }).resize();
 });
