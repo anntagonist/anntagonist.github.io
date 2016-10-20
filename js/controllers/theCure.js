133862afc2642fbda197b68d378001e9df11cadb
anntagonist <andrew.benavides14@alumni.colostate.edu>
2016-10-20 14:12:36 -0600

    added a directive for Character social media profiles

--- js/controllers/theCure.js
+++ js/controllers/theCure.js
@@ -4,30 +4,54 @@ app.controller('theCureCtrl', function($scope){
         description: 'You have found the cure to zombie-idous.'
     };
 
-    $scope.details = [
+//    $scope.details = [
+//        {
+//            heading: "Not Only Infected",
+//            body: ["The infected are not the only thing that is dangerous.",
+//            "The group will need to defend themselves not only from the infected, but the elements, wild animals, and other humans.",
+//            "Do what is necessary to stay alive, and keep the cure out of harms way."
+//            ],
+//            img: "img/baby.jpg"
+//        },
+//        {
+//            heading: "Protect Yourself",
+//            body:["Build traps & tools, and seek shelter for the cold nights.",
+//            "Use weapons to defend yourself from those who pose as a threat.",
+//            "Trust no one."
+//            ],
+//            img: "img/baby.jpg"
+//        },
+//        {
+//            heading: "Kill or Cure",
+//            body: ["You may attempt to kill or save anyone or thing that you come across.",
+//            "You may find benefits to saving someone, but there comes security knowing they no longer pose a threat.",
+//            "The choice is yours, what are your priorities?"
+//            ],
+//            img: "img/baby.jpg"
+//        },
+//    ];
+
+    $scope.characters = [
         {
-            heading: "Not Only Infected",
-            body: ["The infected are not the only thing that is dangerous.",
-            "The group will need to defend themselves not only from the infected, but the elements, wild animals, and other humans.",
-            "Do what is necessary to stay alive, and keep the cure out of harms way."
-            ],
-            img: "img/baby.jpg"
+            name: 'Ann Gables',
+            age: 26,
+            sex: 'Female',
+            rStatus: 'In a Relationship',
+            img: "img/baby.jpg",
         },
         {
-            heading: "Protect Yourself",
-            body:["Build traps & tools, and seek shelter for the cold nights.",
-            "Use weapons to defend yourself from those who pose as a threat.",
-            "Trust no one."
-            ],
-            img: "img/baby.jpg"
+            name: 'Johnathan Roads',
+            age: 28,
+            sex: 'Male',
+            rStatus: 'Single',
+            img: "img/baby.jpg",
         },
         {
-            heading: "Kill or Cure",
-            body: ["You may attempt to kill or save anyone or thing that you come across.",
-            "You may find benefits to saving someone, but there comes security knowing they no longer pose a threat.",
-            "The choice is yours, what are your priorities?"
-            ],
-            img: "img/baby.jpg"
+            name: 'Michael Gables',
+            age: 30,
+            sex: 'Male',
+            rStatus: 'Married',
+            img: "img/baby.jpg",
         },
     ];
 
@@ -42,4 +66,14 @@ app.controller('theCureCtrl', function($scope){
     $scope.setTab = function(tab){
         $scope.currentTab = tab;
     }
-});
+})
+
+.directive('characterProfile', function(){
+     return {
+         restrict: 'E',
+         scope: {
+             character: '=info',
+         },
+         templateUrl: 'views/profile.html'
+     };
+ });
