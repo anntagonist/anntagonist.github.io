30b93c1d355b7d6b23efaa5651f6cab7256bef98
anntagonist <andrew.benavides14@alumni.colostate.edu>
2016-10-21 08:52:13 -0600

    added more info to characters

--- js/controllers/theCure.js
+++ js/controllers/theCure.js
@@ -4,33 +4,6 @@ app.controller('theCureCtrl', function($scope){
         description: 'You have found the cure to zombie-idous.'
     };
 
-//    $scope.details = [
-//        {
-//            heading: "Not Only Infected",
-//            body: ["The infected are not the only thing that is dangerous.",
-//            "The group will need to defend themselves not only from the infected, but the elements, wild animals, and other humans.",
-//            "Do what is necessary to stay alive, and keep the cure out of harms way."
-//            ],
-//            img: "img/baby.jpg"
-//        },
-//        {
-//            heading: "Protect Yourself",
-//            body:["Build traps & tools, and seek shelter for the cold nights.",
-//            "Use weapons to defend yourself from those who pose as a threat.",
-//            "Trust no one."
-//            ],
-//            img: "img/baby.jpg"
-//        },
-//        {
-//            heading: "Kill or Cure",
-//            body: ["You may attempt to kill or save anyone or thing that you come across.",
-//            "You may find benefits to saving someone, but there comes security knowing they no longer pose a threat.",
-//            "The choice is yours, what are your priorities?"
-//            ],
-//            img: "img/baby.jpg"
-//        },
-//    ];
-
     $scope.characters = [
         {
             name: 'Ann Gables',
@@ -38,13 +11,15 @@ app.controller('theCureCtrl', function($scope){
             sex: 'Female',
             rStatus: 'In a Relationship',
             img: "img/baby.jpg",
+            occupation: "Graduate Student at FCU",
         },
         {
             name: 'Johnathan Roads',
             age: 28,
-            sex: 'Male',
+            sex: 'Absolutely',
             rStatus: 'Single',
             img: "img/baby.jpg",
+            occupation: 'Unemployed',
         },
         {
             name: 'Michael Gables',
@@ -52,6 +27,7 @@ app.controller('theCureCtrl', function($scope){
             sex: 'Male',
             rStatus: 'Married',
             img: "img/baby.jpg",
+            occupation: "Forest Ranger - RMNP",
         },
         {
             name: 'Samantha Booty',
@@ -59,6 +35,7 @@ app.controller('theCureCtrl', function($scope){
             sex: 'Female',
             rStatus: 'Widowed',
             img: 'img/baby.jpg',
+            occupation: 'Troop 523 Mother',
         },
     ];
 
